874d2fb143efda6877c3bf33f006f96a23c65df2
Derek Gloudemans <35013478+DerekGloudemans@users.noreply.github.com>
2018-12-18 13:50:59 -0600

    Update game_manager.py
    Initial Commit

---
+++ game_manager.py
@@ -0,0 +1,19 @@
+# Blokus Game Manager
+# Create a playable representation of the game Blokus with necessary functions 
+# for human and AI players to interact with the game
+
+# Piecelist Object
+# A list of lists, where each list represents all possible orientations for a single piece
+
+# Piece Object
+# represents offset coordinates from base location of a piece
+# must be able to translate, rotate and flip pieces
+
+# Board Object
+# represents the game and all played pieces; fully describes the state as seen by players
+# possibly also contains the piecelists for the 4 players
+# implements logicl behind valid moves
+
+# Game Object
+# manages the board, players, and turns, and returns final score of game at the end of the game
+
